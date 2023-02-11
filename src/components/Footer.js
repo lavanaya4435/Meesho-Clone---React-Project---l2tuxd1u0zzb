9bd3c8dff15063848da75391d1cc401660942bc1
Lavanya Reddy <113541450+lavanaya4435@users.noreply.github.com>
2023-02-11 23:11:10 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,236 @@
+import React from 'react'
+import './home.css';
+
+const Footer = () => {
+  return (
+    <div>
+                  <div className="footer">
+                          {/* <div className='list'>
+                                  <h2>Shop Non-Stop on Meesho</h2>
+                                  <p>Trusted by more than 1 Crore Indians<br></br>
+                                  Cash on Delivery | Free Delivery</p>
+                          </div>
+                          <div className='list'>
+            <p>Careers</p>
+            <p>Become a Supplier</p>
+            <p>Our Influencer Program</p>
+            <p>Hall of Fame</p>
+          </div>
+          <div className='list'>
+            <p>Legal and Policies</p>
+            <p>Meesho Tech Blog</p>
+            <p>Notices and Returns</p>
+          </div> */}
+                     
+                                          <h5>More About Meesho</h5>
+                                         
+                          <hr />
+                          
+                          <h5>Meesho: Affordable Online Shopping at Your Fingertips</h5>
+
+                          <p>There are many benefits of shopping online. You can take your time and look at different options to find exactly what
+                                  you want. It's easy to compare prices online and find exactly what you are looking for. And now with Meesho,
+                                  you can shop for anything you want at the lowest prices in the market.  Even if you want to shop for
+                                  cool gifts for your friends and family, there are many options that you can find on the Internet. </p>
+                          
+                          <h5>A Huge Selection of Products Across All Categories</h5>
+
+                          <p>We have a vast inventory of products ranging from apparel to cosmetics to home utility and kitchen products and more.
+                                  With over 50 lakh products and 650+ product categories, Meesho is sure to have everything you need.
+                                  In our latest collections, you will find all the popular items at an affordable price, so you can be confident you're getting the best deal. Whether you're in the market for new clothes, accessories,
+                                  or just some daily-use items for home, Meesho has what you need.</p><br/>
+                          <h5>Latest Women's Fashion Is Right Here</h5><br />
+                          
+                          <h5>Women’s Ethnic Wear Collection</h5>
+                          <p>When it comes to <span className='pink'>women's ethnic wear</span>, we have everything you need to find the perfect
+                                  outfit for any occasion. Whether you're looking for traditional sarees and blouses, or
+                                  something more modern like suits or casual <span className='pink'>Kurtis</span>, we have it all. And with our stunning collection
+                                  of accessories, footwear, and <span className='pink'>jewellery sets</span>, it's easy to put together a complete look that will turn heads at
+                                  your next wedding or festive celebration. So come and shop with us today!
+                          </p><br />
+                          <h5>Western Wear for Women</h5>
+
+                          <p>Meesho is the best place to find fashionable and affordable western wear for women.
+                                  With a constantly updating inventory of casual, formal, and partywear dresses, jeans,
+                                  tops, <span className='pink'>T-shirts</span>, skirts, and more, Meesho has all the latest trending outfits you're looking for--plus
+                                  funky jewelry and accessories. You'll never have to worry about overspending or not being able to find
+                                  something you love!
+                          </p>
+                          <h5>Accessories, Jewellery, and Footwear</h5><br/>
+                          <p>You may have your favorite outfit picked out but accessories can really add a sense of style
+                                  and individuality that make you stand out! With so many types of accessories available,
+                                  it can be difficult to find the perfect piece for yourself. Fortunately, Meesho has a wide
+                                  variety of jewellery sets,<span className='pink'>handbags</span> , belts, heels, casual footwear,<span className='pink'>watches</span> , etc. With so many options,
+                                  you'll be sure to find the perfect accessories for any outfit!
+                          </p><br/>
+                          <h5>Men's Ethnic Wear</h5><br />
+                          <p>
+                                  Do you need a new Sherwani? Are you heading to a wedding and want to look your best?
+                                  If you answered yes, look no further than our latest men’s ethnic wear collection! Whether you want
+                                  a Kurta set, Pajamas, Sherwani set, or any other Indian attire,
+                                  we have an array of budget-friendly styles that are sure to fit your needs.</p><br />
+                          <h4>Largest Collection of Menswear and Accessories </h4><br></br>
+
+
+                           <h5>
+          An Exciting Range of Home Essentials, Kitchen Products, Electronics,
+          and more Electronics and Accessories
+        </h5>
+        <p>
+          If you are looking for affordable gadgets and everyday essentials like
+          <span className='pink'> chargers</span>, <span className='pink'>headphones</span>,{" "}
+          <span className='pink'>data cables</span>, etc, we have a huge selection you can
+          browse. With great deals and sales happening all the time, your wallet
+          will be happy, too. Check out the deals we've picked especially for
+          you, and you'll find great gifts for everyone on your list. Less is
+          more with our <span className='pink'>electronic items</span>, and you'll feel the
+          savings, too.
+        </p>
+        <h5>Kitchenware</h5>
+        <p >
+          We carry all of the essentials that you'll need to spice up your
+          kitchen. You'll have a wide variety of utensils and cookware to choose
+          from, so you could pick something that fits your needs and your
+          budget. We also have kitchen accessories like storage{" "}
+          <span className='pink'>jars and containers</span>, small appliances like{" "}
+          <span className='pink'>juicers</span>,<span>mixers</span>,{" "}
+          <span className='pink'>induction cooktops</span> that will make your cooking a whole
+          lot easier! We have a wide range of serveware and cutlery including
+          glasses, dinner sets, knives to choose from, so you'll be able to find
+          what you need at a great price.
+        </p>
+        <h5>Home Essentials</h5>
+        <p>
+          We know how crucial it is to give your home a makeover every once in a
+          while. However, it is important to ensure that you're investing your
+          money in the right products. With Meesho, you'll find the best prices
+          and the best selections, where we have home furnishing items like
+          bedsheets, <span className='pink'>curtains</span>, <span className='pink'>blankets</span>, etc in every
+          color you could possibly want and we're always making sure that you
+          can get your hands on the best home decor items, with the biggest
+          discounts. So be sure to check out our home decor section.{" "}
+        </p>
+        <h5>Sports, Health &amp; Fitness</h5>
+        <p>
+          It is now more important than ever to keep your body fit and healthy.
+          So start your fitness journey with Meesho. Check out our selection of
+          fitness equipment available at wholesale prices. You will find
+          everything from sportswear to yoga mats to exercise equipment for your
+          home workouts like <span className='pink'>dumbbell sets</span>, resistance bands, etc.{" "}
+        </p>
+        <h5>Stationery Collection</h5>
+        <p>
+          You do not need to spend a lot of money on buying new stationery. We
+          have a large collection of stationery items for various uses and the
+          usage is for everyone, whether you are a student or a working
+          professional. We have a wide variety of beautiful notebooks, diaries,
+          pens, pencils, postcards, sticky notes, and much more.{" "}
+        </p>
+
+                          <h3 className='pink'>Download Meesho App Now</h3><br />
+
+<h5>Become a Reseller: Start Making Money In Three Simple Steps</h5>
+        <p>
+          Now that you've been introduced to Meesho's product categories, it's
+          time to learn about our other offerings. Besides shopping for yourself
+          on the platform, you can also become an entrepreneur and earn cash for
+          what you do best. Here’s how you can begin your entrepreneurial
+          journey. <br />
+          Browse: Use your phone to register a new account, or log in to an
+          existing one at the Meesho app or website. You can read through our
+          catalog and select products that you want at wholesale rates!
+          <br />
+          Share: You can simply send catalogs and product images over Whatsapp,
+          Facebook, or Instagram to your contacts in a single click using the
+          share option. Earn: Once you have shared the catalogs with your
+          prospective customers, you'll soon start getting order requests and
+          inquiries. You can quote product prices with an added margin, which
+          will include a profit for you. Once an order has been placed and paid
+          for, you can place the orders yourself using our established processes
+          that ensure they get delivered to your waiting customers on time.
+          <br />
+          Start Your Online Shopping Journey with Meesho Today You can rest
+          assured that we only deliver the best products to you. You have
+          multiple payment options including debit and credit cards, UPI, and
+          Cash on Delivery. If you're not happy with the quality of your
+          product, send it back and get a full refund without any hassle. Want
+          to learn more? Feel free to send us an email at query@meesho.com. You
+          can also reach out to us on Facebook, Instagram, or Twitter. We're
+          here to help you!
+        </p>
+                          
+                          <h5>Don't Miss Out! Start Shopping Online on Meesho Now</h5><br />
+                          <p>Meesho offers multiple payment options including debit and credit cards, UPI, and 
+                                  COD to help you with a smooth checkout process. If you are unhappy with any of the products 
+                                  you order you can immediately return them and get a full refund with no questions asked. 
+                                  In case you have any queries or concerns, simply send us an email at query@meesho.com.
+                                 Or you can contact us on social media through Facebook, Instagram, or Twitter. We're here to help you! 
+                          </p><br />
+                          <h3 className='pink'>Online Shopping</h3><br/>
+
+                          <h5>Home & Living</h5>
+                          <p className="pink">Curtains & Sheers | Round Cushions | Sofa Covers | Cotton Bedsheets | Bedding Sets |
+                                  Pillow Covers | Decorative Items | Duvet Covers | Blankets | Home Temple | Yoga mats | Bean Bags |
+                                  Single Bed Sheet |
+                                  Cushions | Face Towels | Cotton Towel | Trays | Coasters</p>
+                          <h5>Men Ethnicwear</h5>
+                          <p className="pink">Men Dhoti Kurtas | Men Indo Western Dresses | Men Velvet Sherwanis | Men Dhotis</p>
+  <h5>Women Western Wear</h5>
+          <p className='pink'>
+            Tops | Dresses | Women Tshirts | Gowns | Skirts | Women Trousers |
+            Women Jeans | Women Sweatshirts | Shrugs | Capris | Women Sweaters |
+            Cardigans | Palazzos | Leggings | Tights | Jumpsuits | Kaftans |
+            Maternity Wear | Women Shorts | Jeggings | Nightdress | Women Night
+            Suits | Women Briefs | Lingerie Sets | Bra | Stockings | Camisoles |
+            Bathrobes | Babydolls | Women Shapewear | Women Pyjamas | Swimwear |
+            Women Jackets | Women Thermal Set
+          </p>
+                          <h5>Women Ethnicwear</h5>
+                          <p className="pink">Silk Saree | Puff Sleeve Blouses | Chikankari Kurtis | Designer Lehenga | Sharara
+                                  | Phulkari Dupatta | Petticoats | Abaya | Black Kurta | Velvet Shawls | Blouse Piece | Kurti Fabric
+                                  | Designer Suits</p>
+                          
+<h5>Women Footwear</h5>
+          <p className='pink'>
+            Sandals | Women Slippers | Women Casual Shoes | Women Flats | Women
+            Floaters | Bellies &amp; Ballerinas | Juttis &amp; Mojaris
+          </p>
+          <h5>Women Accessories</h5>
+          <p className='pink'>
+            Women Watches | Mangalsutras | Rings | Handbags | Umbrellas | Women
+            Sunglasses | Jewellery Set | Women Slingbags | Trolley Bags | Hijabs
+            | Socks | Clutches | Hair Accessories | Maangtika | Pouches | Duffle
+            Bags | Handkerchiefs | Bindis | Waist Bags | Rucksacks | Briefcases
+            | Small Travel Bags | Potlis | Nosepins | Bangles &amp; Bracelets |
+            Anklets &amp; Toe Rings | Earrings &amp; Studs | Necklaces &amp;
+            Chains | Scarves &amp; Stoles
+          </p>
+
+                          <h5>Kids</h5>
+
+                          <p className="pink">Kids Lunch Boxes | Kids Ethnic Jackets | Kids Blazers | Rompers | Kids Kurtis & Kurtas | Kids Salwar Suit Sets
+                                  | Kids Ethnic Gowns | Tshirts Boys | Girls Stylish Tops</p>
+                          
+                          <h5>Baby</h5>
+                        <p className='pink'>Baby Pillows | Baby Towels | Baby Hanging Cradle | Baby Sleeping Bag | Baby Blanket</p>
+                          
+                          <h5>Home & Kitchen</h5>
+                          <p className='pink'>Toaster | Mixer Grinder | Cookers | Juicer | Food Processors | Roti Maker | Sandwich Maker |
+                                  Kettles | Hand Blender | Table Cover | Napkins Holder | Kitchen Mat | Kitchen Towel | Aprons
+                                  | Kitchen Gloves | Racks & Holders | Spice Racks | Jars & Container | Water Bottles | Thermos & Flasks |
+                                  Lunch Boxes | Salt & Pepper Shakers | Knives | Food Strainers | Choppers | Mashers | Graters | Cutlery |
+                                  Chopping Boards | Kitchen Scissors | Ice Cube Trays | Measuring Cups | Tongs | Ice Cream Scoops | Funnels
+                                  | Trivets
+                          </p>
+                          <h5>Personal Care & Wellness</h5>
+                          <p className="pink">Tummy trimmer | Makeup Kits | Hair Color | Hair Steamers |
+                                  Hair Care | Oral Care | Beard Oil | Deodorants | Eye Lenses | Soap | Oximeter | Bb Cream | Body Scrub
+                                                  | Olive Oils | Talcum Powder | Face Steamers | Compact | Nail Cutter | Lip Balm</p>
+                                         
+                         
+                  </div>
+    </div>
+  )
+}
+
+export default Footer
