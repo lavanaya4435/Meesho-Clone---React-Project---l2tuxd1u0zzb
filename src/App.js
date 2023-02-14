5f3d982585659a57b1878d538cc607b45921536b
Lavanya Reddy <113541450+lavanaya4435@users.noreply.github.com>
2023-02-14 23:17:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,95 @@
+// // import React from "react";
+// import Navbar from "./components/Navbar/Navbar";
+// import Home from "./components/Home";
+// // // import "./App.css";
+// // // import { BrowserRouter as Router, Route, Link, Routes ,Switch } from 'react-router-dom';
+// // // import {  Switch, Route,Routes } from 'react-router-dom';
+// import Products from "./components/Products";
+// import Product from "./components/Product";
+// import Login from "./components/Login";
+
+// // // import { Route } from "react-router";
+// // import { Route } from "react-router-dom";
+// // import routes from "./components/AllRoutes";
+
+
+// // const App = () => {
+// //   return (
+// //     <>
+// //       {routes.map((route, index) => (
+// //         <Route
+// //           key={index}
+// //           path={route.path}
+// //           exact
+// //           component={route.component}
+// //         />
+// //       ))}
+// //     </>
+// //   );
+// // };
+
+// // export default App;
+ 
+
+// // // const App = () => {
+// // //   return (
+// // //     <>
+// // //       <Navbar />
+      
+  
+// // //       <Routes>
+// // //         <Route path="/home"> <Home /></Route>
+// // //         <Route path="/products"><Products /></Route>
+// // //         {/* <Route path="/product/:id"><Product /></Route> */}
+// // //         <Route path="product/:id" element={<Product />} />
+// // //         <Route path="/login"><Login /></Route>
+// // //         <Product />
+// // //       </Routes>
+       
+      
+       
+      
+// // //     </>
+// // //   );
+// // // };
+
+// // // export default App;
+
+
+
+
 // import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home";
-// // import "./App.css";
-// // import { BrowserRouter as Router, Route, Link, Routes ,Switch } from 'react-router-dom';
-// // import {  Switch, Route,Routes } from 'react-router-dom';
-import Products from "./components/Products";
-import Product from "./components/Product";
-import Login from "./components/Login";
-
-// // import { Route } from "react-router";
 // import { Route } from "react-router-dom";
-// import routes from "./components/AllRoutes";
+// import Cart from "./components/Cart/Cart";
 
+// const routes = [
+//   {
+//     path: "/home",
+//     component: Home
+//   },
+//   {
+//     path: "/products",
+//     component: Products
+//   },
+//   {
+//     path: "/product/:id",
+//     component: Product
+//   },
+//   {
+//     path: "/cart",
+//     component:  Cart
+//  },
+//   {
+//     path: "/login",
+//     component: Login
+//   }
+// ];
 
 // const App = () => {
 //   return (
 //     <>
+//        <Navbar />
+
 //       {routes.map((route, index) => (
 //         <Route
 //           key={index}
@@ -29,42 +103,22 @@ import Login from "./components/Login";
 // };
 
 // export default App;
- 
-
-// // const App = () => {
-// //   return (
-// //     <>
-// //       <Navbar />
-      
-  
-// //       <Routes>
-// //         <Route path="/home"> <Home /></Route>
-// //         <Route path="/products"><Products /></Route>
-// //         {/* <Route path="/product/:id"><Product /></Route> */}
-// //         <Route path="product/:id" element={<Product />} />
-// //         <Route path="/login"><Login /></Route>
-// //         <Product />
-// //       </Routes>
-       
-      
-       
-      
-// //     </>
-// //   );
-// // };
-
-// // export default App;
-
 
 
 
 import React from "react";
-import { Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Product from "./components/Product";
+// import Login from "./components/Login";
 import Cart from "./components/Cart/Cart";
+// import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const routes = [
   {
-    path: "/home",
+    path: "/",
     component: Home
   },
   {
@@ -77,29 +131,37 @@ const routes = [
   },
   {
     path: "/cart",
-    component:  Cart
- },
-  {
-    path: "/login",
-    component: Login
-  }
+    component: Cart
+  },
+  
 ];
 
 const App = () => {
+
+
+  
   return (
     <>
-       <Navbar />
-
-      {routes.map((route, index) => (
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+       
+        <Route path="/products" element={<Products />} /> 
+         <Route path="product/:id" element={<Product />} />
+         <Route path="/cart" element ={<Cart />} /> 
+      {/* {routes.map((route, index) => (
         <Route
           key={index}
           path={route.path}
           exact
           component={route.component}
         />
-      ))}
+      ))} */}
+        </Routes>
     </>
   );
 };
 
 export default App;
+
